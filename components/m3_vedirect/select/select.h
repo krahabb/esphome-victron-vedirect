ea4fb750bba15071209e359700bbf585888a7458
krahabb <13969600+krahabb@users.noreply.github.com>
2025-11-26 15:35:31 +0100

    Improve hiding/overriding (i.e. hacking) of base SelectTraits in select entity

--- components/m3_vedirect/select/select.h
+++ components/m3_vedirect/select/select.h
@@ -24,9 +24,42 @@ class Select final : public WritableRegister, public esphome::select::Select {
   /// @return the newly created Select entity
   static Register *build_entity(Manager *manager, const REG_DEF *reg_def, const char *name);
 
+  // Hack the base SelectTraits to allow direct non-const access
+  // to the options vector, so that we can update/mantain it as needed.
+  // Since we maintain our 'options' list in ENUM_DEF, we need to
+  // override the base class member to fill it with our data.
+  // We can't however avoid a bit of data duplication since the array is maintained
+  // both in ENUM_DEF and in SelectTraits::options. Since 2025.11.0 the FixedVector
+  // implementation avoids storing std::string in its copy and this is better than nothing
+  // so we're only duplicating an array of pointers. Still, preferrable, would be to have a
+  // 'data provider' interface in SelectTraits to avoid duplicating data at all.
+#if ESPHOME_VERSION_CODE >= VERSION_CODE(2025, 11, 0)
+  // See https://github.com/esphome/esphome/pull/11772
+  typedef FixedVector<const char *> options_type;
+#else
+  typedef std::vector<std::string> options_type;
+#endif
+  /// @brief Our custom SelectTraits implementation overrides accessor to allow dynamic
+  /// options modifications
+  class Traits : public esphome::select::SelectTraits {
+   protected:
+    friend class Select;
+    inline options_type &options() { return this->options_; }
+  };
+  /// @brief A fake SelectTraits-like implementation that discards options configuration during component setup
+  /// This will be used through member hiding to avoid initial setup of the base class 'traits' member
+  class FakeTraits {
+   public:
+    void set_options(const std::initializer_list<const char *> &options) {}
+    void set_options(const options_type &options) {}
+  };
+  FakeTraits traits;
+
  protected:
   friend class Manager;
   ENUM_DEF::enum_t enum_value_{ENUM_DEF::VALUE_UNKNOWN};
+
+  inline Traits &traits_() { return reinterpret_cast<Traits &>(this->esphome::select::Select::traits); }
 
   void link_disconnected_() override;
   void init_reg_def_() override;
@@ -50,20 +83,6 @@ class Select final : public WritableRegister, public esphome::select::Select {
   static void parse_text_default_(Register *hex_register, const char *text_value);
   static void parse_text_enum_(Register *hex_register, const char *text_value);
 #endif
-
-#if ESPHOME_VERSION_CODE >= VERSION_CODE(2025, 11, 0)
-  // See https://github.com/esphome/esphome/pull/11772
-  typedef FixedVector<const char *> options_type;
-#else
-  typedef std::vector<std::string> options_type;
-#endif
-  // Hack the basic SelectTraits to allow dynamic management
-  // of options from our Select entity without always copying/moving
-  class SelectTraits : public esphome::select::SelectTraits {
-   public:
-    inline options_type &options() { return this->options_; }
-  };
-  inline SelectTraits &traits_() { return reinterpret_cast<SelectTraits &>(this->traits); }
 
   // optimized publish_state bypassing index checks since we're mantaining our
   // own 'options' source of truth in enum_def_
